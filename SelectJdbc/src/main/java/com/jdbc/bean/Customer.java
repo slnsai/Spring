22df0431fb21d9df459cd595bc0fba5776c28fa6
slnsai <43165158+slnsai@users.noreply.github.com>
2018-10-07 12:23:07 +0530

    customer class

---
+++ SelectJdbc/src/main/java/com/jdbc/bean/Customer.java
@@ -0,0 +1,28 @@
+package com.jdbc.bean;
+
+public class Customer {
+	private int id;
+	private String name;
+	protected String age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	
+
+}
