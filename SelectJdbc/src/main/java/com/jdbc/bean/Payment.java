fed9f5b7f981282e69d03810a3f59ca7c01d1e42
slnsai <43165158+slnsai@users.noreply.github.com>
2018-10-07 15:18:51 +0530

    paymentclass

---
+++ SelectJdbc/src/main/java/com/jdbc/bean/Payment.java
@@ -0,0 +1,21 @@
+package com.jdbc.bean;
+
+public class Payment {
+	protected int paymentId;
+	protected String cardType;
+	public int getPaymentId() {
+		return paymentId;
+	}
+	public void setPaymentId(int paymentId) {
+		this.paymentId = paymentId;
+	}
+	public String getCardType() {
+		return cardType;
+	}
+	public void setCardType(String cardType) {
+		this.cardType = cardType;
+	}
+	
+	
+
+}
